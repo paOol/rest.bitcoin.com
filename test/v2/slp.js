2eb4c20758f3f514d50f92c3fbbe800fa184912a
Gabriel Cardona <gabriel@bitcoin.com>
2019-03-11 21:01:36 -0700

    Comment.

--- test/v2/slp.js
+++ test/v2/slp.js
@@ -860,7 +860,7 @@ describe("#SLP", () => {
         "df808a41672a0a0ae6475b44f272a107bc9961b90f29dc918d71301f24fe92fb"
 
       const result = await balancesForTokenSingle(req, res)
-      console.log(`result: ${util.inspect(result)}`)
+      // console.log(`result: ${util.inspect(result)}`)
 
       assert.hasAllKeys(result.balances[0], ["address", "tokenBalance"])
     })

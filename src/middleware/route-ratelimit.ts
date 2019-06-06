67ad1dea5bc1050dead3887239ab537f4c0a9b05
Gabriel Cardona <gabriel@bitcoin.com>
2019-06-06 18:04:27 +0900

    Testing websockets.

--- src/middleware/route-ratelimit.ts
+++ src/middleware/route-ratelimit.ts
@@ -36,8 +36,8 @@ const routeRateLimit = function(
   next: express.NextFunction
 ) {
   // Create a res.locals object if not passed in.
-  if(!req.locals) req.locals = {}
-  
+  if (!req.locals) req.locals = {}
+
   // Disable rate limiting if 0 passed from RATE_LIMIT_MAX_REQUESTS
   if (maxRequests === 0) return next()
 

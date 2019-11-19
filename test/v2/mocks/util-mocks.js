d74aedbfe8a89b39a9105b9092be2e5828960764
Chris Troutner <chris.troutner@gmail.com>
2019-11-18 16:08:09 -0800

    Updating mocks for unit tests

--- test/v2/mocks/util-mocks.js
+++ test/v2/mocks/util-mocks.js
@@ -5,8 +5,6 @@ const mockAddress = {
   isvalid: true,
   address: "bchtest:qqqk4y6lsl5da64sg5qc3xezmplyu5kmpyz2ysaa5y",
   scriptPubKey: "76a914016a935f87e8deeab04501889b22d87e4e52db0988ac",
-  ismine: false,
-  iswatchonly: false,
   isscript: false
 }
 
